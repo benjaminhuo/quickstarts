3860490a616c6c6c44a17bcf2428c3163a5d8509
benjaminhuo <benjamin@yunify.com>
2021-05-11 18:23:25 +0800

    Add keda autoscaling to dapr input bindings
    Signed-off-by: benjaminhuo <benjamin@yunify.com>

--- bindings/nodeapp/app.js
+++ bindings/nodeapp/app.js
@@ -12,10 +12,16 @@ app.use(bodyParser.json());
 
 const port = 3000;
 
+function wait(second) {
+    let ChildProcess_ExecSync = require('child_process').execSync;
+    ChildProcess_ExecSync('sleep ' + second);
+};
+
 app.post('/sample-topic', (req, res) => {
-    console.log("Hello from Kafka!");
+//    console.log("Hello from Kafka!");
     console.log(req.body);
     res.status(200).send();
+    wait(1);
 });
 
 app.listen(port, () => console.log(`Node App listening on port ${port}!`));
